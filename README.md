721e6e6ec419cf0a2d1cc27acdf7205d4685c748
Samuel Mimram <smimram@gmail.com>
2023-09-01 17:16:15 +0200

    Process.

--- README.md
+++ README.md
@@ -1,6 +1,35 @@
 # Machine learning audio effects
 
-This is my own take at coding the [guitar amplifier model based on
+Trying to learn machine learning for audio effects. This is my own take at
+coding the [guitar amplifier model based on
 LSTMs](https://github.com/Alec-Wright/Automated-GuitarAmpModelling) (also
 [described
-here](https://towardsdatascience.com/neural-networks-for-real-time-audio-stateful-lstm-b534babeae5d)).
+here](https://towardsdatascience.com/neural-networks-for-real-time-audio-stateful-lstm-b534babeae5d)). In
+OCaml.
+
+## Usage
+
+In order to learn an effect you need both an original sound file (say
+`source.wav`) and a processed sound file (say `target.wav`). You can start
+learning with
+
+```shell
+leffect -s source.wav -t target.wav
+```
+
+This will learn the effect. While doing so it will also produce an `output.wav`
+file (name can be changed with `-o`) resulting of the processing of the input
+file by the current network. Some other useful parameters are
+
+- `--rate`: the learning rate
+- `--size`: the size of the network
+
+At the end, it produces an `effect.json` file (the name can be changed with the
+`--json` flag).
+
+In order to process a file with an already learned effect (whose parameters are
+stored in `effect.json`), you can simply do
+
+```shell
+leffect -i file.wav -o output.wav
+```
--- src/leffect.ml
+++ src/leffect.ml
@@ -7,6 +7,7 @@ let () =
   let rate = ref 0.1 in
   let size = ref 20 in
   Arg.parse [
+    "-i", Arg.Set_string source, "Input file.";
     "-s", Arg.Set_string source, "Source file.";
     "-t", Arg.Set_string target, "Target file.";
     "-o", Arg.Set_string output, "Output file.";
@@ -15,31 +16,54 @@ let () =
     "--size", Arg.Set_int size, "Size of the network."
   ] (fun _ -> ()) "learn [options]";
   if !source = "" then error "Please specify an input file.";
-  if !target = "" then error "Please specify a target file.";
-  Random.self_init ();
-  let source = WAV.openfile !source in
-  let target = WAV.openfile !target in
-  let samples = WAV.samples source in
-  let output = WAV.Writer.openfile ~channels:1 ~samplerate:(WAV.samplerate source) !output in
-  let net = Net.create (`WrightGRU !size) in
-  try
-    let i = ref 0 in
-    while true do
-      Printf.printf "\rProcessing: %.00f%%%!" (100. *. float !i /. float samples);
-      incr i;
-      let x = WAV.sample_float source in
-      let x = x.(0) in
-      let yc = Net.process net x in
-      let yt = WAV.sample_float target in
-      let yt = yt.(0) in
-      WAV.Writer.sample_float output yc;
-      (* Printf.printf "S: %.02f\tT: %.02f\tC: %.02f\n" x yt yc; *)
-      Net.descent net yt !rate
-    done;
-  with
-  | End_of_file ->
-    Printf.printf "\rDone!\n%!";
-    if !json <> "" then
-      let oc = open_out !json in
-      Net.to_json net |> Yojson.Basic.pretty_to_channel oc;
-      close_out oc
+  if !target = "" then
+    (
+      let source = WAV.openfile !source in
+      let channels = WAV.channels source in
+      let samplerate = WAV.samplerate source in
+      let samples = WAV.samples source in
+      let output = WAV.Writer.openfile ~channels ~samplerate !output in
+      let json = Yojson.Basic.from_file !json in
+      let net = Array.init channels (fun _ -> Net.of_json json) in
+      try
+        let i = ref 0 in
+        while true do
+          Printf.printf "\rProcessing: %.00f%%%!" (100. *. float !i /. float samples);
+          incr i;
+          let x = WAV.sample_float source in
+          let y = Array.init channels (fun c -> Net.process net.(c) x.(c)) in
+          WAV.Writer.samples_float output y
+        done
+      with End_of_file -> Printf.printf "\rDone!\n%!"
+    )
+  else
+    (
+      Random.self_init ();
+      let source = WAV.openfile !source in
+      let target = WAV.openfile !target in
+      let samples = WAV.samples source in
+      let output = WAV.Writer.openfile ~channels:1 ~samplerate:(WAV.samplerate source) !output in
+      let net = Net.create (`WrightGRU !size) in
+      try
+        let i = ref 0 in
+        ignore (Sys.signal Sys.sigint (Signal_handle (fun _ -> raise End_of_file)));
+        while true do
+          Printf.printf "\rProcessing: %.00f%%%!" (100. *. float !i /. float samples);
+          incr i;
+          let x = WAV.sample_float source in
+          let x = x.(0) in
+          let yc = Net.process net x in
+          let yt = WAV.sample_float target in
+          let yt = yt.(0) in
+          WAV.Writer.sample_float output yc;
+          (* Printf.printf "S: %.02f\tT: %.02f\tC: %.02f\n" x yt yc; *)
+          Net.descent net yt !rate
+        done;
+      with
+      | End_of_file ->
+        Printf.printf "\rDone! Writing to %s.\n%!" !json;
+        if !json <> "" then
+          let oc = open_out !json in
+          Net.to_json net |> Yojson.Basic.pretty_to_channel oc;
+          close_out oc
+    )
